4495dd8e3da9bd09964870f57b70cebaf9dee3f3
Aleksei Ogoltcov <aaogoltcov@mai.ru>
2023-08-13 17:08:52 +0300

    Исправил наименование полей и аргументов

--- src/main/java/org/Nasa.java
+++ src/main/java/org/Nasa.java
@@ -6,9 +6,9 @@ public class Nasa {
     private final String copyright;
     private final String date;
     private final String explanation;
-    private final String hdurl;
-    private final String media_type;
-    private final String service_version;
+    private final String hdUrl;
+    private final String mediaType;
+    private final String serviceVersion;
     private final String title;
     private final String url;
 
@@ -16,18 +16,18 @@ public class Nasa {
         @JsonProperty("copyright")String copyright,
         @JsonProperty("date")String date,
         @JsonProperty("explanation")String explanation,
-        @JsonProperty("hdurl")String hdurl,
-        @JsonProperty("media_type")String media_type,
-        @JsonProperty("service_version")String service_version,
+        @JsonProperty("hdurl")String hdUrl,
+        @JsonProperty("media_type")String mediaType,
+        @JsonProperty("service_version")String serviceVersion,
         @JsonProperty("title")String title,
         @JsonProperty("url")String url
     ) {
         this.copyright = copyright;
         this.date = date;
         this.explanation = explanation;
-        this.hdurl = hdurl;
-        this.media_type = media_type;
-        this.service_version = service_version;
+        this.hdUrl = hdUrl;
+        this.mediaType = mediaType;
+        this.serviceVersion = serviceVersion;
         this.title = title;
         this.url = url;
     }
@@ -38,9 +38,9 @@ public class Nasa {
             "copyright='" + copyright + '\'' +
             ", date='" + date + '\'' +
             ", explanation='" + explanation + '\'' +
-            ", hdurl='" + hdurl + '\'' +
-            ", media_type='" + media_type + '\'' +
-            ", service_version='" + service_version + '\'' +
+            ", hdurl='" + hdUrl + '\'' +
+            ", media_type='" + mediaType + '\'' +
+            ", service_version='" + serviceVersion + '\'' +
             ", title='" + title + '\'' +
             ", url='" + url + '\'' +
             '}';
